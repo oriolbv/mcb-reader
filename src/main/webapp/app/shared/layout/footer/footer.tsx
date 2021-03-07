41464ea3f0175a7d27e37cd008a7479cd7719f93
Oriol Burgaya <oriolbv@gmail.com>
2021-03-07 11:43:18 +0100

    Application generated

---
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -0,0 +1,17 @@
+import './footer.scss';
+
+import React from 'react';
+
+import { Col, Row } from 'reactstrap';
+
+const Footer = props => (
+  <div className="footer page-content">
+    <Row>
+      <Col md="12">
+        <p>Oriol Burgaya Verdaguer</p>
+      </Col>
+    </Row>
+  </div>
+);
+
+export default Footer;
